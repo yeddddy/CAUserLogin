b4727c2fd5b34b1b3d7a152a07eabadd0676b5be
enzeyang <jedi.ygsw@gmail.com>
2024-10-21 17:40:30 -0400

    Task 1 done

--- src/main/java/app/MainWithInMemory.java
+++ src/main/java/app/MainWithInMemory.java
@@ -1,12 +1,7 @@
 package app;
 
-import java.awt.CardLayout;
-
-import javax.swing.JFrame;
-import javax.swing.JPanel;
-import javax.swing.WindowConstants;
-
 import data_access.DBUserDataAccessObject;
+import data_access.InMemoryUserDataAccessObject;
 import entity.CommonUserFactory;
 import interface_adapter.ViewManagerModel;
 import interface_adapter.logged_in.LoggedInViewModel;
@@ -17,10 +12,13 @@ import view.LoginView;
 import view.SignupView;
 import view.ViewManager;
 
+import javax.swing.*;
+import java.awt.*;
+
 /**
  * The version of Main with an external database used to persist user data.
  */
-public class MainWithDB {
+public class MainWithInMemory {
 
     /**
      * The main method for starting the program with an external database used to persist user data.
@@ -53,7 +51,7 @@ public class MainWithDB {
         final SignupViewModel signupViewModel = new SignupViewModel();
 
         // TODO Task 1.1 in a copy of this file, change this line to use the in-memory DAO.
-        final DBUserDataAccessObject userDataAccessObject = new DBUserDataAccessObject(new CommonUserFactory());
+        final InMemoryUserDataAccessObject userDataAccessObject = new InMemoryUserDataAccessObject();
 
         final SignupView signupView = SignupUseCaseFactory.create(viewManagerModel, loginViewModel,
                                                                   signupViewModel, userDataAccessObject);
